958f35878e2f5191082826d3448693287c753dda
Unknown <i.mr.tuz@gmail.com>
2016-12-10 23:40:54 +0300

    Сюда надо добавить вычитание

--- src/AceProject/Main.java
+++ src/AceProject/Main.java
@@ -1,8 +1,28 @@
 package AceProject;
 
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите первое число");
+        int a = scanner.nextInt();
+        System.out.println("Введите действие 1-сложение,2-вычииание");
+
+        int znak = scanner.nextInt();
+
+        System.out.println("Введите второе число");
+        int b = scanner.nextInt();
+
+        if (znak == 1){
+            int result = a+b;
+            System.out.println(result);
+        }
+
+
+
     }
 }
